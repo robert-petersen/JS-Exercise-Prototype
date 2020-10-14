5bbe2643026f6ab9b0269978e0cfe7e2bc936cd2
Robert Petersen <robert.petersen808@gmail.com>
2020-10-14 14:27:37 -0500

    finished mvp

--- index.js
+++ index.js
@@ -39,9 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.eat = function(food){
+  if(this.stomach.length <= 9){
+  this.stomach.push(food);
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+
 
 /*
   TASK 2
@@ -57,9 +72,19 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
+}
+// start of strech goals :)
+// Car.prototype.drive = function(distance){
+
+// }
 
 /*
   TASK 3
@@ -68,18 +93,27 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global Binding: When in the global scope 'this' will refer to the window object. When you don't refer to something correctly you will most likey do this.
+
+  2. Implicit Binding: 'this' will refer to the object on the left when you call with a "." / like example.key will apply all this.key as example.key
+
+  3. Explicit Binding: when you use .call and .apply you are explictly binding 'this' to that object
+
+  4. new binding: when you use new it expects a constructor function which will bind this to that new created object
 */
 
 
